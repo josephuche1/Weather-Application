0ebb7b6ada1fe227eabc480406b6bc91ef1409c7
PARSIVALJ <uchejesusboy@gmail.com>
2023-09-30 09:54:17 +0300

    added code to run the application

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, ()=>{
+    console.log(`Server running from port ${port}`);
+})
