9f4271e391d55730c46a05829a361545b123e166
PARSIVALJ <uchejesusboy@gmail.com>
2023-11-05 22:41:13 +0200

    Use passport-local-mongoose to authenticate users

--- server.js
+++ server.js
@@ -4,14 +4,26 @@ import bodyParser from "body-parser";
 import axios from "axios";
 import mongoose from "mongoose";
 import mongodb from "mongodb";
-import bcrypt from "bcrypt";
-
+import session from "express-session";
+import passport from "passport"
+import passportLocalMongoose from "passport-local-mongoose";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+//set up session
+app.use(session({
+    secret:"weatherApplication1",
+    resave:false,
+    saveUninitialized:false
+}));
+// initialize passport
+app.use(passport.initialize());
+// set up passport to work with express session
+app.use(passport.session());
+
 
 mongoose.connect("mongodb+srv://admin-joseph:olisa312@cluster0.phfwo7l.mongodb.net/weatherApp")
    .then(() => {
@@ -28,52 +40,49 @@ const userSchema = new mongoose.Schema({
     history:[String]
 });
 
-const User =  mongoose.model("user", userSchema);
+userSchema.plugin(passportLocalMongoose);
+
+const User =  new mongoose.model("user", userSchema);
+
+
+// set up passport-local dependency, this will help us serialize and desialize user information 
+// during authentication. it is used with the mongoose model.
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", async (req, res) => {
     res.render("index1.ejs");
 });
 
-app.post("/login", async (req, res) => {
-    try{
-        const user = await User.find({username: req.body.username});
-        if(user || user.length !== 0){
-            res.render("index2.ejs");
-        }
-        else{
-            res.redirect("/");
-        }
-    } catch(err){
-        console.error(`An error has occured: ${err.message}`);
-        res.redirect("/")
+app.get("/:username", (req, res) => {
+    if(req.isAuthenticated){
+        res.render("index2.ejs");
+    }else{
+        res.redirect("/");
     }
+    
+});
 
+app.post("/login", async (req, res) => {
+    
 });
 
 app.post("/signup", async (req,res) => {
-   try{
-     const user = await User.find({username: req.body.username}); 
-     if(!user || user.length === 0){
-        const newUser = new User({
-            username: req.body.username,
-            password: await bcrypt.hash(req.body.password, 15),
-            location: req.body.location,
-            history:[]
-        });
-        await newUser.save();
-        res.render("index2.ejs");
-     }
-     else{
-        res.redirect("/");
-     }
-   }catch(err){
-     console.error(`An error occured: ${err.message}`);
-     res.redirect("/")
-   }
+    User.register({username:req.body.username, location:req.body.location, history:[]}, req.body.password, (err, user) => {
+        if(err){
+            console.error(`an error has occurred: ${err.message}`);
+            res.redirect("/");
+        }
+        else{
+            passport.authenticate("local")(req,res, () => {
+                res.redirect(`/${req.body.username}`);
+            })
+        }
+    })
 });
 
-app.post("")
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
