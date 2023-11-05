2a0aa14eb8b3a4f84b7cd6831c820787b0bc39d2
PARSIVALJ <uchejesusboy@gmail.com>
2023-11-05 17:22:05 +0200

    Add routes to allow users to login and sign up in the wesite and store their information in the database

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import mongoose from "mongoose";
+import mongodb from "mongodb";
+import bcrypt from "bcrypt";
 
 
 const app = express();
@@ -10,11 +13,67 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb+srv://admin-joseph:olisa312@cluster0.phfwo7l.mongodb.net/weatherApp")
+   .then(() => {
+        console.log("Connected to database successfully.")
+   })
+   .catch((err) => {
+      console.log(`Failed to connect to database: ${err.message}`);
+   })
+
+const userSchema = new mongoose.Schema({
+    username:String,
+    password:String,
+    location:String,
+    history:[String]
+});
+
+const User =  mongoose.model("user", userSchema);
 
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     res.render("index1.ejs");
 });
+
+app.post("/login", async (req, res) => {
+    try{
+        const user = await User.find({username: req.body.username});
+        if(user || user.length !== 0){
+            res.render("index2.ejs");
+        }
+        else{
+            res.redirect("/");
+        }
+    } catch(err){
+        console.error(`An error has occured: ${err.message}`);
+        res.redirect("/")
+    }
+
+});
+
+app.post("/signup", async (req,res) => {
+   try{
+     const user = await User.find({username: req.body.username}); 
+     if(!user || user.length === 0){
+        const newUser = new User({
+            username: req.body.username,
+            password: await bcrypt.hash(req.body.password, 15),
+            location: req.body.location,
+            history:[]
+        });
+        await newUser.save();
+        res.render("index2.ejs");
+     }
+     else{
+        res.redirect("/");
+     }
+   }catch(err){
+     console.error(`An error occured: ${err.message}`);
+     res.redirect("/")
+   }
+});
+
+app.post("")
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
