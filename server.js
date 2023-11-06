667e2bc11bca0dbaf45d0b759bfc11727569331b
PARSIVALJ <uchejesusboy@gmail.com>
2023-11-06 02:58:02 +0200

    Create function to convert weathercode to text

--- server.js
+++ server.js
@@ -72,8 +72,6 @@ function currentMore(daily, locationData){
     return current;
 }
 
-
-
 function hourlyEdit(hourly){
     const d = new Date();
     const hour = d.getHours();
@@ -100,12 +98,41 @@ function hourlyEdit(hourly){
     return arrayHourly;
 }
 
-function dailyEdit(daily){
-    let weatherInterpretation = [];
-    for(let i = 0; i < 100; i++){
-        weatherInterpretation[i] = "";
-    }
+function WeatherInterpretation(daily){
+    const descriptions = {
+        0: "Clear sky",
+        1: "Mainly clear",
+        2: "Partly clear",
+        3: "Overcast",
+        45: "Fog",
+        48: "Depositing rime fog",
+        51: "Light drizzle",
+        53: "Moderate drizzle",
+        55: "Dense drizzle",
+        56: "Light freezing drizzle",
+        57: "Dense freezing drizzle",
+        61: "Slight Rain",
+        63: "Moderate rain",
+        65: "Heavy rain",
+        66: "Light freezing rain",
+        67: "Heavy freezing rain",
+        71: "Slight snow fall",
+        73: "Moderate snow fall",
+        75: "Heavy snow fall",
+        77: "Snow grains",
+        80: "Slight rain showers",
+        81: "Moderate rain showers",
+        82: "Violent rain showers",
+        85: "Slight snow showers",
+        86: "Heavy snow showers",
+        95: "Thunderstorm",
+        99: "Thunderstorm (Hail)"
+    };
+
+    return descriptions;
 }
+
+
 
 
 
